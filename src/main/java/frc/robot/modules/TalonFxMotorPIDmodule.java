fdd568782191a12c6611fab26a765a000c47e297
Moon Jam <wu.messi.wu@gmail.com>
2024-02-26 14:31:41 +0800

    so trash

--- src/main/java/frc/robot/modules/TalonFxMotorPIDmodule.java
+++ src/main/java/frc/robot/modules/TalonFxMotorPIDmodule.java
@@ -4,70 +4,102 @@
 
 package frc.robot.modules;
 
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 import frc.robot.Constants.PortID;
 import frc.robot.Constants;
 
 public class TalonFxMotorPIDmodule {
   public TalonFX motor;
-  private double cvtTicks_velocity = 1;
-  private double cvtTicks_position = 1;
-  /** Creates a new ExampleSubsystem. */
+  public TalonFXConfiguration Config = new TalonFXConfiguration();
+  DutyCycleOut driveDutyCycle = new DutyCycleOut(0);
+  VelocityVoltage driveVelocity = new VelocityVoltage(0);
+  PositionVoltage anglePosition = new PositionVoltage(0);
 
-  public TalonFxMotorPIDmodule(PortID id, NeutralMode NeutralMode, FeedbackDevice feedbackDevice) {
-    motor = TalonFXInitModule(id, NeutralMode, feedbackDevice);
+  InvertedValue bool2InvertedValue(boolean bool) {
+    return bool ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
   }
 
-  public void cv2Ticks_velocity(double cvtTicks_velocity){
+  /** Creates a new ExampleSubsystem. */
+  public TalonFxMotorPIDmodule(PortID id, NeutralModeValue NeutralMode) {
+    Config.MotorOutput.Inverted = bool2InvertedValue(id.reversed);
+    Config.MotorOutput.NeutralMode = NeutralMode;
+    Config.Slot0.kP = id.kP;
+    Config.Slot0.kI = id.kI;
+    Config.Slot0.kD = id.kD;
+
+    Config.CurrentLimits.SupplyCurrentLimitEnable = id.EnableCurrentLimit;
+    Config.CurrentLimits.SupplyCurrentLimit = id.CurrentLimit;
+    Config.CurrentLimits.SupplyCurrentThreshold = id.CurrentThreshold;
+    Config.CurrentLimits.SupplyTimeThreshold = id.CurrentThresholdTime;
+
+    Config.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = (id.ramp_rate);
+    Config.OpenLoopRamps.VoltageOpenLoopRampPeriod = (id.ramp_rate);
+    Config.ClosedLoopRamps.DutyCycleClosedLoopRampPeriod = (id.ramp_rate);
+    Config.ClosedLoopRamps.DutyCycleClosedLoopRampPeriod = (id.ramp_rate);
+
+    Config.Feedback.SensorToMechanismRatio = 1;
+    motor.getConfigurator().apply(Config);
+    motor.setVoltage(10);
+  }
+
+  public void cv2Ticks_velocity(double cvtTicks_velocity) {
     this.cvtTicks_velocity = cvtTicks_velocity;
   }
 
-  public void cv2Ticks_position(double cvtTicks_position){
+  public void cv2Ticks_position(double cvtTicks_position) {
     this.cvtTicks_position = cvtTicks_position;
   }
 
-  public void setVelocity(double Unit){
-    motor.set(ControlMode.Velocity, (int)(Unit* cvtTicks_velocity));
+  public void setVelocity(double Unit) {
+    motor.set(ControlMode.Velocity, (int) (Unit * cvtTicks_velocity));
   }
-  
-  public void stop(){
+
+  public void stop() {
     motor.set(ControlMode.PercentOutput, 0);
   }
 
-  public void set_position(double Unit){
-    motor.set(ControlMode.Position, (int)(Unit* cvtTicks_position));
+  public void set_position(double Unit) {
+    motor.set(ControlMode.Position, (int) (Unit * cvtTicks_position));
   }
 
-  public double get_Velocity(){
-    return motor.getSelectedSensorVelocity()/cvtTicks_velocity;
+  public double get_Velocity() {
+    return motor.getSelectedSensorVelocity() / cvtTicks_velocity;
   }
-  
-  public void setPercentOutput(double PercentOutput){
-    if(Math.abs(PercentOutput)<0.05) PercentOutput = 0;
+
+  public void setPercentOutput(double PercentOutput) {
+    if (Math.abs(PercentOutput) < 0.05)
+      PercentOutput = 0;
     motor.set(ControlMode.PercentOutput, PercentOutput);
   }
 
-  public void reset_position(){
+  public void reset_position() {
     motor.setSelectedSensorPosition(0);
   }
 
-  public void reset_current_position(double Unit){
-    motor.setSelectedSensorPosition((int)(Unit* cvtTicks_position));
+  public void reset_current_position(double Unit) {
+    motor.setSelectedSensorPosition((int) (Unit * cvtTicks_position));
   }
 
-  public double get_position(){
-    return motor.getSelectedSensorPosition()/cvtTicks_position;
+  public double get_position() {
+    return motor.getSelectedSensorPosition() / cvtTicks_position;
   }
 
-  public double get_OutputPercent(){
+  public double get_OutputPercent() {
     return motor.getMotorOutputPercent();
   }
 
-  private TalonFX TalonFXInitModule(PortID id, NeutralMode NeutralMode, FeedbackDevice feedbackDevice) {
+  private TalonFX TalonFXInitModule(PortID id, NeutralMode NeutralMode) {
     TalonFX talonfx = new TalonFX(id.port);
     talonfx.configFactoryDefault();
     talonfx.setInverted(id.reversed);
@@ -81,7 +113,7 @@ public class TalonFxMotorPIDmodule {
     talonfx.config_kF(0, id.kF, Constants.kTIMEOUT);
     talonfx.config_IntegralZone(0, id.I_Zone, Constants.kTIMEOUT);
     talonfx.configAllowableClosedloopError(0, id.allow_error, Constants.kTIMEOUT);
-    
+
     return talonfx;
   }
 
